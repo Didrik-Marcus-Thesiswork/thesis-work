cc6025b11465c1cedb6a6e47fb9f4d1561ac4fb1
marcusvilhelmsson <55685699+marcusvilhelmsson@users.noreply.github.com>
2022-04-22 10:40:50 +0200

    Update init.sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,7 +1,25 @@
 /* WRITE QUERIES HERE */
+CREATE TABLE IF NOT EXISTS libraries(
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    street VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE IF NOT EXISTS books(
     id INT AUTO_INCREMENT NOT NULL,
-    author VARCHAR(50) NOT NULL,
-    title VARCHAR(250) NOT NULL,
-    PRIMARY KEY (id)
+    library_id INT NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    release_year INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (library_id) REFERENCES libraries(id)
+);
+
+CREATE TABLE IF NOT EXISTS librarians(
+    id INT AUTO_INCREMENT NOT NULL,
+    library_id INT NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    age INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (library_id) REFERENCES libraries(id)
 );
